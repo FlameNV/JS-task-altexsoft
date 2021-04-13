02cc5b809a995b945da5a8c4f088381d47489d24
FlameNV <56412379+FlameNV@users.noreply.github.com>
2021-04-13 15:46:56 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,38 @@
+const optionSelector = document.querySelectorAll("option");
+const text = document.querySelector("p");
+
+function createLetters() {
+  let letter = "";
+  let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  for (let i = 0; i < 5; i++) {
+    letter += alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+    alphabet = alphabet.replace(letter, "");
+    optionSelector[i].innerHTML = letter;
+    letter = "";
+  }
+}
+createLetters();
+
+function renderResult() {
+  const letterOptions = document.getElementById("letterSelector").options;
+  const selectedLetter = document.getElementById("letterSelector")
+    .selectedIndex;
+  const selectedLetterText = letterOptions[selectedLetter].text;
+  text.innerText = " ";
+  let counter = 1;
+  fetch("./js/list.json")
+    .then((response) => response.json())
+    .then(function (fetchJson) {
+      const obj = fetchJson;
+      for (let i = 0; i < obj.length; i++) {
+        let nameFirstLetter = obj[i].name.charAt(0);
+        if (selectedLetterText === nameFirstLetter) {
+          text.innerText += `${counter}. ${obj[i].name} \n`;
+          ++counter;
+        }
+      }
+      if (counter === 1) {
+        text.innerText = "Sorry, did not match";
+      }
+    });
+}
