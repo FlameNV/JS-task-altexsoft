9fe8a31d3f1d84aee068c2ab76353e9b44f462fb
FlameNV <56412379+FlameNV@users.noreply.github.com>
2021-04-17 12:01:42 +0300

    Fixed selector

--- js/main.js
+++ js/main.js
@@ -4,10 +4,10 @@ const text = document.querySelector("p");
 function createLetters() {
   let letter = "";
   let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0, j = 1; i < 5; i++, j++) {
     letter += alphabet.charAt(Math.floor(Math.random() * alphabet.length));
     alphabet = alphabet.replace(letter, "");
-    optionSelector[i].innerHTML = letter;
+    optionSelector[j].innerHTML = letter;
     letter = "";
   }
 }
@@ -24,15 +24,19 @@ function renderResult() {
     .then((response) => response.json())
     .then(function (fetchJson) {
       const obj = fetchJson;
-      for (let i = 0; i < obj.length; i++) {
-        let nameFirstLetter = obj[i].name.charAt(0);
-        if (selectedLetterText === nameFirstLetter) {
-          text.innerText += `${counter}. ${obj[i].name} \n`;
-          ++counter;
+      if (selectedLetterText === "Choose") {
+        text.innerText = "Choose a letter";
+      } else {
+        for (let i = 0; i < obj.length; i++) {
+          let nameFirstLetter = obj[i].name.charAt(0);
+          if (selectedLetterText === nameFirstLetter) {
+            text.innerText += `${counter}. ${obj[i].name} \n`;
+            ++counter;
+          }
         }
-      }
-      if (counter === 1) {
-        text.innerText = "Sorry, did not match";
+        if (counter === 1) {
+          text.innerText = "Sorry, did not match";
+        }
       }
     });
 }
